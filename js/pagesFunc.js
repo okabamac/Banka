dfe1ff6ee8537033d8cc0495064b8d37881bbef4
Okaba Mac <markokaba99@gmail.com>
2019-03-28 09:44:29 +0100

    Dahboard content space :cake: Built a responsive card content space The card content space will hold the pages functionality Made sure its theme follows the general black theme of the app

--- js/pagesFunc.js
+++ js/pagesFunc.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector('.hamburger-icon');
 const sideMenu = document.querySelector('#sideMenu');
+const mainContent = document.querySelector('#mainContent');
 
 let show = false;
 
@@ -16,4 +17,44 @@ hamburger.addEventListener('click', () => {
     show = !show;
     toggle(sideMenu);
     hamburger.classList.toggle('close');
+    if(mainContent){
+        mainContent.classList.toggle('contentOpened');
+    }
 });
+
+
+
+window.addEventListener("resize",  () => {
+    console.log(sideMenu.style.display);
+  if (window.innerWidth > 700) {
+              if(mainContent.classList.contains('contentOpened')){
+                  mainContent.classList.remove('contentOpened');
+                  sideMenu.style.display = 'block';
+              }
+            else {
+                if (hamburger.classList.contains('close')) {
+                    hamburger.classList.remove('close');
+                }
+                if(hamburger.classList.contains('close') == false && window.innerWidth < 700 ){
+                    sideMenu.style.display = 'none';
+                }
+            }
+          }
+}, true);
+
+
+
+(function () {
+    let greeting;
+    const hour = new Date().getHours();
+    if (hour < 12) {
+        greeting = 'Goodmorning,';
+    }
+    else if ((hour >= 12) && (hour < 16)) {
+        greeting = 'Goodafternoon,';
+    }
+    else {
+        greeting = 'Goodevening,';
+    }
+    document.getElementById('day').innerHTML = greeting;
+})();
