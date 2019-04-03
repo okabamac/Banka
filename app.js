51ac1d03e227e83dd8cb246c5d5a6a3a38e3f4a2
Okaba Mac <markokaba99@gmail.com>
2019-04-03 13:47:43 +0100

    Server setup :cake: Set up the server with express Created basic api versioning

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const morgan = require('morgan');
+
+const app = express();
+
+// const userRoute = require('./src/routes/userRoute');
+
+app.use(morgan('dev'));
+
+
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  }),
+);
+
+app.use(express.json());
+
+app.use('/api/v1/user', userRoute);
+app.use('/api/v1/account', accountRoute);
+app.use('/api/v1/transaction', transactionRoute);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    status: error.status,
+    error: error.message,
+  });
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log('zzzzzzzzzzzz');
+const http = require('http');
+
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(port, () => console.info(`Application running on port ${port}`));
