4708fd4e65130653d92cd1c0005a02a206b8fe20
Okaba Mac <markokaba99@gmail.com>
2019-04-07 08:11:50 +0100

    Revert "Merge pull request #13 from okabamac/staff-cashier-can-credit-account"
    This reverts commit 69b3b0cca69584d946f248888abe7002663ca4ff, reversing
    changes made to 5f7f6d1a7e43af996eff3b0d0cadb7210b81daf8.

--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const app = require('./app');
-
-const port = process.env.PORT || 3000;
-const server = http.createServer(app);
-
-server.listen(port, () => console.info(`Application running on port ${port}`));
