926b90d5be93f4d1d24db0643a204c2c22d9bf53
Okaba Mac <markokaba99@gmail.com>
2019-04-08 17:48:31 +0100

    Added more data to the models

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -8,6 +8,24 @@ const users = [
     type: 'staff',
     isAdmin: true,
   },
+  {
+    id: 123457,
+    email: 'kelly00233@gmail.com',
+    firstName: 'Kell',
+    lastName: 'Jones',
+    password: '$2b$10$MWLVVYvFBQHvmjuRLFS6xuvZxpiN2KZJyQbCLb/lXxGZ2vGVmY2vy',
+    type: 'staff',
+    isAdmin: true,
+  },
+  {
+    id: 123458,
+    email: 'mike@gmail.com',
+    firstName: 'Mesu',
+    lastName: 'Joy',
+    password: '$2b$10$MWLVVYvFBQHvmjuRLFS6xuvZxpiN2KZJyQbCLb/lXxGZ2vGVmY2vy',
+    type: 'staff',
+    isAdmin: true,
+  },
 ];
 
 module.exports = users;
