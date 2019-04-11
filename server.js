84fae91cffdc9dbeb0a628b35d8a97ba3b16c63a
Okaba Mac <markokaba99@gmail.com>
2019-04-11 19:19:53 +0100

    Added environment variables :house: Abstracted config file to use env variables Moved port to env file Put env in .gitignore

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const http = require('http');
 
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const { port } = require('./config');
+
 const server = http.createServer(app);
 
 server.listen(port, () => console.info(`Application running on port ${port}`));
